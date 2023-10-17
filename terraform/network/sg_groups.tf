30ea700737c79095346b013f596927b223cecca7
Rafael Ferreira <80534010+rafaelbrandaof@users.noreply.github.com>
2023-10-17 09:47:29 +0200

    Update sg_groups.tf

--- terraform/network/sg_groups.tf
+++ terraform/network/sg_groups.tf
@@ -1,11 +1,25 @@
 resource "aws_security_group" "polkadot_security_group" {
-  name        = "Nginx"
+  name        = "Polkadot"
   description = "Allow HTTP/SSH traffic from the internet"
   vpc_id      = "${aws_vpc.vpc.id}"
 
   ingress {
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9933
+    to_port     = 9933
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9615
+    to_port     = 9615
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
